0f021c53f26965f4cc55687de1761df9e01305f3
kajaldhanotia20 <kajaldhanotia1997@gmail.com>
2022-04-22 14:29:42 -0700

    adding app.js
    `

--- 281frontend/src/App.js
+++ 281frontend/src/App.js
@@ -1,10 +1,27 @@
-import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./components/navBar";
+import UserRoute from "./components/common/userRoute";
+import AdminRoute from "./components/common/adminRoute";
+import { Route, Redirect, Switch } from "react-router-dom";
+
+import "./App.css";
 import Login from "./components/login";
 import Logout from "./components/logout";
 import Register from "./components/register";
-import { Route, Navigate, Routes } from "react-router-dom";
+
+
+import AdminDashboard from "./components/adminDashboard";
+
+
+import { socketUrl } from "./config.json";
+import { getJwt } from "./services/authService";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { io } from "socket.io-client";
+export const socket = io(socketUrl, {
+  query: {
+    jwtToken: getJwt(),
+  },
+});
 
 class App extends Component {
   state = {};
@@ -13,19 +30,23 @@ class App extends Component {
       <React.Fragment>
         <NavBar></NavBar>
         <main className="container" style={{ paddingTop: "80px" }}>
-          <Routes>
+          <Switch>
             <Route path="/login" component={Login}></Route>
             <Route path="/logout" component={Logout}></Route>
             <Route path="/register" component={Register}></Route>
            
-            <Navigate from="/" exact to="/login"></Navigate>
-          </Routes>
+           
+
+            <AdminRoute
+              path="/dashboard"
+              component={AdminDashboard}
+            ></AdminRoute>
+            <Redirect from="/" exact to="/login"></Redirect>
+          </Switch>
         </main>
       </React.Fragment>
     );
   }
 }
-
-
 
 export default App;
